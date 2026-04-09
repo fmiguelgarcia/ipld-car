7a40e43a3b6a03344477fa50d0693cd4b9b0c658
fmiguelgarcia <francisco.miguel.garcia.rodriguez@gmail.com>
2026-04-09 14:00:48 +0200

    Fix: CARGO_MANIFEST_DIR bug

--- ipld-car/src/test_helpers.rs
+++ ipld-car/src/test_helpers.rs
@@ -23,12 +23,12 @@ where
 
 /// It loads the file `<project>/resources/tests/<name>`
 pub fn test_file<P: AsRef<Path>>(name: P) -> BufReader<File> {
-	base_test_file(name, &[])
+	base_test_file(name, "")
 }
 
 /// It loads the file `<project>/resources/tests/fixtures/<name>`
 pub fn test_fixtures_file<P: AsRef<Path>>(name: P) -> BufReader<File> {
-	base_test_file(name, &["fixtures"])
+	base_test_file(name, "fixtures/")
 }
 
 /// It loads the associated roots file (extension `.roots`) of the given test file.
@@ -36,10 +36,7 @@ pub fn roots_test_file<P: AsRef<Path>>(name: P) -> Vec<String> {
 	let mut root_name = name.as_ref().to_path_buf();
 	root_name.set_extension("roots");
 
-	base_test_file(root_name, &["exp", "roots"])
-		.lines()
-		.collect::<Result<Vec<_>, _>>()
-		.expect(INVALID_LINE)
+	base_test_file(root_name, "exp/roots/").lines().collect::<Result<Vec<_>, _>>().expect(INVALID_LINE)
 }
 
 /// It loads the associated block IDs file (extension `.blockIds`) of the given test file.
@@ -47,28 +44,34 @@ pub fn block_ids_test_file<P: AsRef<Path>>(name: P) -> Vec<String> {
 	let mut block_id_name = name.as_ref().to_path_buf();
 	block_id_name.set_extension("blockIds");
 
-	base_test_file(block_id_name, &["exp", "block_ids"])
+	base_test_file(block_id_name, "exp/block_ids/")
 		.lines()
 		.collect::<Result<Vec<_>, _>>()
 		.expect(INVALID_LINE)
 }
 
+/// Get the base path for resources used in test.
+///
+/// NOTE: On previous `cargo` versions (or nightly), `CARGO_MANIFEST_DIR` points to
+/// workspace's folder instead of the individual crate's folder.
 pub fn base_test_path() -> PathBuf {
 	let cargo_manifest_dir = env::var("CARGO_MANIFEST_DIR").expect(MISSING_CARGO_MANIFEST);
-	Path::new(&cargo_manifest_dir).join("..").join("resources").join("tests")
+
+	let on_crate_dir = Path::new(&cargo_manifest_dir).join("../resources/tests");
+	if on_crate_dir.exists() {
+		return on_crate_dir;
+	}
+
+	let on_workspace_dir = Path::new(&cargo_manifest_dir).join("resources/tests");
+	on_workspace_dir
 }
 
 pub fn test_fixtures_path() -> PathBuf {
 	base_test_path().join("fixtures")
 }
 
-pub fn base_test_file<P: AsRef<Path>>(name: P, relative_paths: &[&str]) -> BufReader<File> {
-	let mut path = base_test_path();
-	for sub in relative_paths {
-		path = path.join(sub);
-	}
-	let path = path.join(name);
-
+pub fn base_test_file<P: AsRef<Path>>(name: P, relative_path: &str) -> BufReader<File> {
+	let path = base_test_path().join(relative_path).join(name);
 	let file = File::open(path).expect(FILE_OPEN_FAIL);
 	BufReader::new(file)
 }
